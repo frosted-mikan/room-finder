eb4cc861b3cdfdc1fca87196fa8092225f710316
ahuo <amberhuo2012@gmail.com>
2022-04-17 15:20:10 -0400

    get rid of alert

--- script.js
+++ script.js
@@ -215,7 +215,7 @@ function initMap() {
 
       //Set icon to face the correct direction
       console.log("lat,lng: ", lat, lng)
-      alert("heading: ", hdng); // hdng is null -- device problem?
+      // alert("heading: ", hdng); // hdng is null -- device problem?
       const head_icon = {
         path: google.maps.SymbolPath.FORWARD_CLOSED_ARROW,
         scale: 5,
